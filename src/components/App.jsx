b4a6581187be76382cacba648ca059f405a6f850
andyfullstack <andriikoliadenko@gmail.com>
2024-01-29 14:27:16 +0200

    filter fix & app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,7 @@
-import { getCurrentUser } from 'store/operations';
-import { useDispatch, useSelector } from 'react-redux';
-import { Suspense, useEffect } from 'react';
-import { fetchContacts } from 'store/contOperations.js';
 import { Route, Routes } from 'react-router-dom';
 import { selectIsLoading } from 'store/root/selectors.js';
-import { selectUser } from 'store/selectors.js';
 import Loader from './Loader/Loader';
-
+import { Suspense } from 'react';
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
 import Contacts from './pages/Contacts/Contacts';
@@ -15,16 +10,10 @@ import Login from './pages/Login/Login';
 import Error from './pages/Error/Error';
 import PrivateRoute from 'route/PrivatRoute';
 import PublicRoute from 'route/PublicRoute';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
   const isLoading = useSelector(selectIsLoading);
-
-  useEffect(() => {
-    !user && dispatch(getCurrentUser());
-    user && dispatch(fetchContacts());
-  }, [dispatch, user]);
 
   return isLoading ? (
     <p>boot page</p>
